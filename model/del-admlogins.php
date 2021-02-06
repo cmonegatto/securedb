61cd01f2982a23b19e4c890284d045e5084c467a
Claudio Monegatto <cmonegatto@gmail.com>
2021-02-06 02:12:59 -0300

    LOG de Registro de alterações das regras para _AUD

--- model/del-admlogins.php
+++ model/del-admlogins.php
@@ -30,11 +30,71 @@ if (isset($_SESSION['msg']) && strlen($_SESSION['msg'])>0 ):
 	exit;	
 endif;
 
+$datetime = date('d/m/Y H:i:s');
+
+if ($player=='OCI'):
+	$datetimestr = "to_char('$datetime','dd-mm-yyyy hh24:mi:ss')";
+	$datetimestr = 'SYSDATE';
+elseif ($player == 'SQLSRV'):
+	$datetimestr = "format('$datetime','dd/MM/yyyy HH:mm:ss')";
+	$datetimestr = 'GETDATE()';
+endif;
+
+
+
+$loginname = strtoupper($_SESSION['s_login']);
+
+$result= $conn->sql(basename(__FILE__), 
+					"INSERT INTO ADM_LOGINS_AUD ( ACTION
+												, ACTION_BY
+												, ACTION_DATE
+												, ID_LOGIN
+												, USERNAME
+												, OSUSER
+												, MACHINE
+												, BEGIN_DATE       
+												, END_DATE
+												, FREETOOLS
+												, SESSIONS_PER_USER
+												, LOG_LOGON
+												, TRACE
+												, CURSOR_SHARING
+												, INIT_PLSQL
+												, COMMENTS
+												, CREATED_BY
+												, CREATED_DATE
+												, LAST_UPDATED_BY
+												, LAST_UPDATED_DATE
+				  							   )
+					SELECT   'DELETE'
+							, '$loginname'
+							, $datetimestr
+							, $id
+							, USERNAME			
+							, OSUSER			
+							, MACHINE			
+							, BEGIN_DATE		
+							, END_DATE			
+							, FREETOOLS		
+							, SESSIONS_PER_USER
+							, LOG_LOGON		
+							, TRACE			
+							, CURSOR_SHARING	
+							, INIT_PLSQL		
+							, COMMENTS			
+							, CREATED_BY		
+							, CREATED_DATE		
+							, LAST_UPDATED_BY	
+							, LAST_UPDATED_DATE
+					  FROM ADM_LOGINS
+					 WHERE id_login = :ID",
+					array(":ID" => $id));
+
+
 $result= $conn->sql(basename(__FILE__), 
 					"DELETE FROM adm_logins 
 					  WHERE id_login = :ID",
 					array(":ID" => $id));
-
 
 header("Location: \admlogins");
 exit;	
